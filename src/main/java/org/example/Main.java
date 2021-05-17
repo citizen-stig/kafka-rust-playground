4711277c85d259aba6c358a94293cc2c2149fe15
Nikolai Golub <nikolay.v.golub@gmail.com>
2021-05-17 17:49:14 +0200

    Add logging

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,14 @@
 package org.example;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class Main {
 
+    static Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("HOI");
+        logger.info("Starting....");
+
     }
 }
